0eb0c42747959612e701e3c21fda648da3b47119
Asiphenombeleko <asiphenombeleko33@gmail.com>
2023-04-20 16:13:17 +0200

    file path error

--- course-cost/test/cost.test.js
+++ course-cost/test/cost.test.js
@@ -1,8 +1,19 @@
-//write a statement that will describe my function
-describe("courseCost function",function(){
-describe("the function should calculate the total cost of the course", function(){
-it ("should return an object with status,code,description,cost and amount due",function(){
+//wite the name of the function
+describe("the courseCost function", function(){
+    //write what is the function required to do 
+it ("should return invalid code if the passed arguement is not a course type ", function (){
+   //write an assert statement telling what the function should return 
+   let result = courseCost("z2", "yy");
+    assert.deepEqual({ status: 'Invalid code' }, result)
+})
+
+it ("should return Invalid day if the day is wraped around a number value ", function(){
+    let result = courseCost("r1", "yy");
+assert.deepEqual({ status: 'Invalid day' }, result)
 
 })
+it("should return success when the code is correct  ", function(){
+    let result = courseCost("r1", "yy")
 })
-}) 
+assert.deepEqual({ status: 'success' }, result)
+})
